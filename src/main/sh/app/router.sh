7c4bc0217a6709dc2540d3fd3d4caaf42b9f2111
lildworks <lildworks@gmail.com>
2023-11-15 12:07:32 +0000

    REFACT: One line router

--- src/main/sh/app/router.sh
+++ src/main/sh/app/router.sh
@@ -1,32 +1,16 @@
 run() {
     if [ "$ldtc_cmd" = "version" ]; then
-        if [ "$ldtc_target" = "print" ]; then
-            doPrintVersion
-        fi
+        if [ "$ldtc_target" = "print" ]; then doPrintVersion; fi
     fi
     if [ "$ldtc_cmd" = "usage" ]; then
-        if [ "$ldtc_target" = "print" ]; then
-            doPrintUsage
-        fi
+        if [ "$ldtc_target" = "print" ]; then doPrintUsage; fi
     fi
     if [ "$ldtc_cmd" = "compile" ]; then
-        if [ "$ldtc_target" = "bash-app" ]; then
-            doCompileBashApplication
-        fi
-        if [ "$ldtc_target" = "bash-application" ]; then
-            doCompileBashApplication
-        fi
-        if [ "$ldtc_target" = "compose" ]; then
-            doCompileDockerCompose
-        fi
-        if [ "$ldtc_target" = "docker-compose" ]; then
-            doCompileDockerCompose
-        fi
-        if [ "$ldtc_target" = "nginx" ]; then
-            doCompileNginxConf
-        fi
-        if [ "$ldtc_target" = "nginx-config" ]; then
-            doCompileNginxConf
-        fi
+        if [ "$ldtc_target" = "bash-app" ]; then doCompileBashApplication; fi
+        if [ "$ldtc_target" = "bash-application" ]; then doCompileBashApplication; fi
+        if [ "$ldtc_target" = "compose" ]; then doCompileDockerCompose; fi
+        if [ "$ldtc_target" = "docker-compose" ]; then doCompileDockerCompose; fi
+        if [ "$ldtc_target" = "nginx" ]; then doCompileNginxConf; fi
+        if [ "$ldtc_target" = "nginx-config" ]; then doCompileNginxConf; fi
     fi
 }
